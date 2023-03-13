2ac46f91ae50b5140fd73a48648fe6e7033e764f
Alexxxiy69 <0705203@gmail.com>
2023-03-13 15:30:15 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,70 +39,8 @@ class _HomeWidgetState extends State<HomeWidget> {
 }
 
 class MyPainter extends CustomPainter {
-  List x = [
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-    300.0,
-  ];
-  List y = [
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-    150.0,
-  ];
+  List x = List.generate(30, (index) => 300.0);
+  List y = List.generate(30, (index) => 150.0);
   int i = 0;
 
   void generate() {
@@ -112,39 +50,9 @@ class MyPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    print(x);
     var pointMode = ui.PointMode.polygon;
-    var points = [
-      Offset(x[0], y[0]),
-      Offset(x[1], y[1]),
-      Offset(x[2], y[2]),
-      Offset(x[3], y[3]),
-      Offset(x[4], y[4]),
-      Offset(x[5], y[5]),
-      Offset(x[6], y[6]),
-      Offset(x[7], y[7]),
-      Offset(x[8], y[8]),
-      Offset(x[9], y[9]),
-      Offset(x[10], y[10]),
-      Offset(x[11], y[11]),
-      Offset(x[12], y[12]),
-      Offset(x[13], y[13]),
-      Offset(x[14], y[14]),
-      Offset(x[15], y[15]),
-      Offset(x[16], y[16]),
-      Offset(x[17], y[17]),
-      Offset(x[18], y[18]),
-      Offset(x[19], y[19]),
-      Offset(x[20], y[20]),
-      Offset(x[21], y[21]),
-      Offset(x[22], y[22]),
-      Offset(x[23], y[23]),
-      Offset(x[24], y[24]),
-      Offset(x[25], y[25]),
-      Offset(x[26], y[26]),
-      Offset(x[27], y[27]),
-      Offset(x[28], y[28]),
-      Offset(x[29], y[29]),
-    ];
+    var points = List.generate(30, (index) => Offset(x[i], y[i]));
 
     for (i; i < 29; i++) {
       print(x[i]);
